34017482a76a8290aad9e2afd21f885c2bee6a98
EmirSoftic <e.softic2001@gmail.com>
2023-05-10 20:05:07 +0200

    All of frontend

---
+++ src/Komponente/NalovLogovaniKorisnikComp.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import '../CSS/NaslovLogovaniKorisnik.css';
+
+function NaslovLogovaniKorisnik() {
+    return (
+        <div>
+            <h1>LEKCIJE</h1>
+            <p>Prvu pomoć treba znati svako!</p>
+        </div>
+    )
+}
+
+export default NaslovLogovaniKorisnik
