912c0c849912537eddcff55c3f1d867d8dc7f884
Iman2605 <mahmutovic.iman@gmail.com>
2023-05-12 23:39:07 +0200

    napravila kartice

--- frontend/src/Komponente/karticaIzgledComp.js
+++ frontend/src/Komponente/karticaIzgledComp.js
@@ -1,12 +1,18 @@
 import '../CSS/KarticaCSS.css';
 
+import Button from '@mui/material/Button';
+
 function Kartica(props) {
     return (
         <div className="card" style={{ backgroundImage: `url(${props.image})` }}>
             <h2>{props.title}</h2>
             <hr />
             <p>{props.content}</p>
-            <button>{props.buttonText}</button>
+            <Button
+                variant="outlined"
+                color="secondary"
+                disabled={false}
+            > Izaberi </Button>
         </div>
     );
 }
